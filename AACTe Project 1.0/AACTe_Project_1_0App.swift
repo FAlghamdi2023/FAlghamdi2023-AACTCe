084febb9a20467596a0ed87b1650704995fc0305
F Alghamdi <falghamdibox@gmail.com>
2023-01-23 16:45:23 +0300

    Initial Commit

---
+++ AACTe Project 1.0/AACTe_Project_1_0App.swift
@@ -0,0 +1,17 @@
+//
+//  AACTe_Project_1_0App.swift
+//  AACTe Project 1.0
+//
+//  Created by Fatimah Alghamdi on 23/01/2023.
+//
+
+import SwiftUI
+
+@main
+struct AACTe_Project_1_0App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
